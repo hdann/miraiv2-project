986324f55a2eac79b0309e108f9e35c980b49f92
Trần Hữu Đan <92423717+hdann@users.noreply.github.com>
2022-02-24 19:22:53 +0700

    Lộn xíu :vvvvvvvvvvv

--- modules/commands/help.js
+++ modules/commands/help.js
@@ -14,7 +14,7 @@ module.exports.config = {
 };
 
 module.exports.languages = {
- vi": {
+" vi": {
 		"moduleInfo": "「 %1 」\n%2\n\n❯ Cách sử dụng: %3\n❯ Thuộc nhóm: %4\n❯ Thời gian chờ: %5 giây(s)\n❯ Quyền hạn: %6\n\n» Module code by %7 «",
 		"helpList": '[ Hiện tại đang có %1 lệnh có thể sử dụng trên bot này, Sử dụng: "%2help nameCommand" để xem chi tiết cách sử dụng! ]"',
 		"user": "Người dùng",
